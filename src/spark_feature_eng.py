69d5aa4ba89da15a86c7df206dc2b400a398993e
vhoudebine <vhoudebine@microsoft.com>
2024-03-28 12:20:37 -0400

    fixed typo in spark code

--- src/spark_feature_eng.py
+++ src/spark_feature_eng.py
@@ -2,8 +2,15 @@ import argparse
 from operator import add
 from pyspark.sql.functions import col, sum, lag
 from pyspark.sql.window import Window
+from pyspark.sql import SparkSession
 import os 
 
+
+
+spark = (
+    SparkSession.builder.appName("AML Dataprep")
+    .getOrCreate()
+)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--raw_data")
@@ -21,5 +28,5 @@ df = df.withColumn("30_d_fuel_type_is_claim_sum", sum(col("is_claim")).over(wind
 
 print (f"Writing to {args.training_data}")
 
-df.write.mode('overwrite').parquet(training_data_path)
+df.write.mode('overwrite').parquet(args.training_data)
 
